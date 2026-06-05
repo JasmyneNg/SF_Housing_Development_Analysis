6e01b3ac2dc35f3ff5912dc04f874f4fb775156c
poffo <Benho1j@outlook.com>
2026-06-05 20:23:53 +0000

    added print statements

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
+#import pandas
 import pandas as pd 
 
-housing_csv=pd.read_csv("housing_dev.csv")
+#import the csv 
+df=pd.read_csv("housing_dev.csv")
+#print(df.columns.tolist())
 
-print(housing_csv.head())
+print(df)
+
+#print unique data 
+#print(df["Zoning District"].unique())
+#print(df["Net Units"].unique())
+
+#print(df["Planning Dist."].unique())
+
+#print the net units 
+#print(df["Net Units"]>=0)
